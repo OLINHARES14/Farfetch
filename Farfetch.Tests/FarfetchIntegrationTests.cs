6b6b6312f94ee46503efad9a59e8c99175c0b73f
OLINHARES14 <mbignacio83@gmail.com>
2018-09-23 23:12:22 -0300

    Implamentação Toggle

--- Farfetch.Tests/FarfetchIntegrationTests.cs
+++ Farfetch.Tests/FarfetchIntegrationTests.cs
@@ -1,79 +1,79 @@
-﻿using Farfetch.Domain.HttpServices;
-using Farfetch.Domain.Models.Entities;
-using Farfetch.Domain.Services.Contracts.Entities;
-using Farfetch.Domain.Services.Contracts.Infra.Data.UoW;
-using Farfetch.Domain.Services.Imp.Tasks;
-using Farfetch.Tests.ModelTests;
-using FluentAssertions;
-using NSubstitute;
-using System.Collections.Generic;
-using System.Net;
-using System.Threading.Tasks;
-using Xunit;
+﻿//using Farfetch.Domain.HttpServices;
+//using Farfetch.Domain.Models.Entities;
+//using Farfetch.Domain.Services.Contracts.Entities;
+//using Farfetch.Domain.Services.Contracts.Infra.Data.UoW;
+//using Farfetch.Domain.Services.Imp.Tasks;
+//using Farfetch.Tests.ModelTests;
+//using FluentAssertions;
+//using NSubstitute;
+//using System.Collections.Generic;
+//using System.Net;
+//using System.Threading.Tasks;
+//using Xunit;
 
-namespace Farfetch.Tests
-{
-    public class FarfetchIntegrationTests
-    {
-        private IUnitOfWork UnitOfWork { get; }
-        private IToggleEntityService ToggleEntityService { get;  }
-        private IServiceRotaEntityService ServiceRotaEntityService { get; }
+//namespace Farfetch.Tests
+//{
+//    public class FarfetchIntegrationTests
+//    {
+//        private IUnitOfWork UnitOfWork { get; }
+//        private IToggleEntityService ToggleEntityService { get;  }
+//        private IServiceRotaEntityService ServiceRotaEntityService { get; }
 
-        ToggleServiceTask task;
+//        ToggleServiceTask task;
 
-        public FarfetchIntegrationTests()
-        {
-            UnitOfWork = Substitute.For<IUnitOfWork>();
-            ToggleEntityService = Substitute.For<IToggleEntityService>();
-            ServiceRotaEntityService = Substitute.For<IServiceRotaEntityService>();
+//        public FarfetchIntegrationTests()
+//        {
+//            UnitOfWork = Substitute.For<IUnitOfWork>();
+//            ToggleEntityService = Substitute.For<IToggleEntityService>();
+//            ServiceRotaEntityService = Substitute.For<IServiceRotaEntityService>();
 
-            task = new ToggleServiceTask(UnitOfWork, ToggleEntityService, ServiceRotaEntityService);
-        }
+//            task = new ToggleServiceTask(UnitOfWork, ToggleEntityService, ServiceRotaEntityService);
+//        }
 
-        #region [ Tasks ]
+//        #region [ Tasks ]
 
 
-        private HttpResult<Toggle> Create(Toggle toggle, List<int> idsServiceRota)
-        {
-            return task.Create(toggle, idsServiceRota);
-        }
+//        private HttpResult<Toggle> Create(Toggle toggle, List<int> idsServiceRota)
+//        {
+//            return task.Create(toggle, idsServiceRota);
+//        }
 
-        private Task<HttpResult<List<Toggle>>> GetAll()
-        {
-            return task.GetAll();
-        }
+//        private Task<HttpResult<List<Toggle>>> GetAll()
+//        {
+//            return task.GetAll();
+//        }
 
-        private Task<HttpResult<Toggle>> Get(int id)
-        {
-            return task.Get(id);
-        }
+//        private Task<HttpResult<Toggle>> Get(int id)
+//        {
+//            return task.Get(id);
+//        }
 
-        private Task<HttpResult<Toggle>> Update(int id, string description, bool flag, List<int> idsServiceRota)
-        {
-            return task.Update(id, description, flag, idsServiceRota);
-        }
+//        private Task<HttpResult<Toggle>> Update(int id, string description, bool flag, List<int> idsServiceRota)
+//        {
+//            return task.Update(id, description, flag, idsServiceRota);
+//        }
 
-        private Task<HttpResult<Toggle>> Delete(int id)
-        {
-            return task.Delete(id);
-        }
+//        private Task<HttpResult<Toggle>> Delete(int id)
+//        {
+//            return task.Delete(id);
+//        }
 
-        #endregion
+//        #endregion
 
-        [Fact]
-        public void Register_order_valido()
-        {
-            var toggleMock = ModelTests.ToggleMock.ToggleValido();
+//        [Fact]
+//        public void Register_order_valido()
+//        {
+//            var toggleMock = ModelTests.ToggleMock.ToggleValido();
 
-            var listaIdsServiceRotaMock = new List<int>();
-            listaIdsServiceRotaMock.Add(ToggleServiceRotaMock.ToggleServiceRotaValido().Id);
+//            var listaIdsServiceRotaMock = new List<int>();
+//            listaIdsServiceRotaMock.Add(ToggleServiceRotaMock.ToggleServiceRotaValido().Id);
 
-            var retornoCreate = Create(toggleMock, listaIdsServiceRotaMock);
-            var id = retornoCreate.Response.Id;
-            var retornoGet = Get(id);
-            var retornoPay = Update(id, retornoCreate.Response.Description, retornoCreate.Response.Flag, listaIdsServiceRotaMock);
+//            var retornoCreate = Create(toggleMock, listaIdsServiceRotaMock);
+//            var id = retornoCreate.Response.Id;
+//            var retornoGet = Get(id);
+//            var retornoPay = Update(id, retornoCreate.Response.Description, retornoCreate.Response.Flag, listaIdsServiceRotaMock);
 
-            retornoPay.Result.HttpStatusCode.Should().Equals(HttpStatusCode.OK);
-        }
-    }
-}
+//            retornoPay.Result.HttpStatusCode.Should().Equals(HttpStatusCode.OK);
+//        }
+//    }
+//}
---
+++ Farfetch.Tests/ToggleUnitTests.cs
@@ -0,0 +1,151 @@
+﻿using Farfetch.Domain.HttpServices;
+using Farfetch.Domain.Models.Entities;
+using Farfetch.Domain.Services.Contracts.Entities;
+using Farfetch.Domain.Services.Contracts.Infra.Data.UoW;
+using Farfetch.Domain.Services.Imp.Tasks;
+using Farfetch.Tests.ModelTests;
+using FluentAssertions;
+using NSubstitute;
+using System.Collections.Generic;
+using System.Net;
+using Xunit;
+
+namespace Farfetch.Tests
+{
+    public class ToggleUnitTests
+    {        
+        private IUnitOfWork UnitOfWork { get; }
+        private IToggleEntityService ToggleEntityService { get; }
+        private IServiceRotaEntityService ServiceRotaEntityService { get; }
+
+        ToggleServiceTask task;
+
+        public ToggleUnitTests()
+        {
+            UnitOfWork = Substitute.For<IUnitOfWork>();
+            ToggleEntityService = Substitute.For<IToggleEntityService>();
+            ServiceRotaEntityService = Substitute.For<IServiceRotaEntityService>();
+
+            task = new ToggleServiceTask(UnitOfWork, ToggleEntityService, ServiceRotaEntityService);
+        }
+
+        #region [ Tasks ]
+
+        private HttpResult<Toggle> Create(Toggle toggle, List<int> idsServiceRota)
+        {   
+            return task.Create(toggle, idsServiceRota);
+        }
+
+        //private Task<HttpResult<List<Toggle>>> GetAll()
+        //{
+        //    return task.GetAll();
+        //}
+
+        //private Task<HttpResult<Toggle>> Get(int id)
+        //{
+        //    return task.Get(id);
+        //}
+
+        //private Task<HttpResult<Toggle>> Update(int id, string description, bool flag, List<int> idsServiceRota)
+        //{
+        //    return task.Update(id, description, flag, idsServiceRota);
+        //}
+
+        //private Task<HttpResult<Toggle>> Delete(int id)
+        //{
+        //    return task.Delete(id);
+        //}
+
+        #endregion
+
+        #region [ Cenarios ]
+
+        [Fact]
+        public Toggle Create_valido()
+        {
+            var toggleMock = ToggleMock.ToggleValido();
+            toggleMock.Id = 0;
+
+            var listaIdsServiceRotaMock = new List<int>();
+            listaIdsServiceRotaMock.Add(ToggleServiceRotaMock.ToggleServiceRotaValido().Id);
+
+            var retorno = Create(toggleMock, listaIdsServiceRotaMock);
+
+            retorno.HttpStatusCode.Should().Equals(HttpStatusCode.Created);
+
+            return retorno.Response;
+        }
+
+        //[Fact]
+        //public void GetAll_valido()
+        //{
+        //    var retorno = GetAll();
+
+        //    retorno.Result.HttpStatusCode.Should().Equals(HttpStatusCode.OK);
+        //}
+
+        //[Fact]
+        //public void Get_valido()
+        //{
+        //    var toggle = Create_valido();
+
+        //    var retorno = Get(toggle.Id);
+
+        //    retorno.Result.HttpStatusCode.Should().Equals(HttpStatusCode.OK);
+        //}
+
+        //[Fact]
+        //public void Get_invalido_id_zero()
+        //{
+        //    var retorno = Get(0);
+
+        //    retorno.Result.HttpStatusCode.Should().Equals((HttpStatusCode)422);
+        //}
+
+        //[Fact]
+        //public void Update_valido()
+        //{
+        //    var toggle = Create_valido();
+
+        //    var listaIdsServiceRotaMock = new List<int>();
+        //    listaIdsServiceRotaMock.Add(toggle.ToggleServiceRotas.FirstOrDefault().Id);
+
+        //    var retorno = Update(toggle.Id, toggle.Description, toggle.Flag, listaIdsServiceRotaMock);
+
+        //    retorno.Result.HttpStatusCode.Should().Equals(HttpStatusCode.OK);
+        //}
+
+        //[Fact]
+        //public void Update_invalido_id_zero()
+        //{
+        //    var toggle = Create_valido();
+
+        //    var listaIdsServiceRotaMock = new List<int>();
+        //    listaIdsServiceRotaMock.Add(toggle.ToggleServiceRotas.FirstOrDefault().Id);
+
+        //    var retorno = Update(0, toggle.Description, toggle.Flag, listaIdsServiceRotaMock);
+
+        //    retorno.Result.HttpStatusCode.Should().Equals((HttpStatusCode)422);
+        //}
+
+        //[Fact]
+        //public void Delete_valido() 
+        //{
+        //    var toggle = Create_valido();
+
+        //    var retorno = Delete(toggle.Id);
+
+        //    retorno.Result.HttpStatusCode.Should().Equals(HttpStatusCode.OK);
+        //}
+
+        //[Fact]
+        //public void Delete_invalido_id_zero()
+        //{
+        //    var retorno = Delete(0);
+
+        //    retorno.Result.HttpStatusCode.Should().Equals((HttpStatusCode)422);
+        //}
+
+        #endregion
+    }
+}
